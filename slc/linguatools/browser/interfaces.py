93b1e62f2b93ea82ec456a8d6f42111edf0dccd4
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-31 15:29:41 +0000

    Add form fields

--- slc/linguatools/browser/interfaces.py
+++ slc/linguatools/browser/interfaces.py
@@ -2,7 +2,6 @@ from zope import interface, schema
 
 class IBaseSchema(interface.Interface):
     """ Base Schema for the edit form. It is dynamically extended by plugins """
-
     title = schema.TextLine(
             title=u"Set a Title", 
             description= \
@@ -17,3 +16,18 @@ class IBaseSchema(interface.Interface):
                 u"Rename an object with old id in this folder to the new id.",
             required=False
             )
+
+    title_from_po = schema.TextLine(
+            title=u"Set the id (shortname) from a .po file.", 
+            description= \
+                u"Set Title or Description on all translations based on the message id of a po file. Specify a message id and a po file domain. The proper translations, if available, will then be set on all translations.",
+            required=False
+            )
+
+    description_from_po = schema.TextLine(
+            title=u"Set the description from a .po file.", 
+            description= \
+                u"Set Title or Description on all translations based on the message id of a po file. Specify a message id and a po file domain. The proper translations, if available, will then be set on all translations.",
+            required=False
+            )
+
