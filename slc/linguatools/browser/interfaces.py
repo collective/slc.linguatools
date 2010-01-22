61d87418e4564c4628dfd55491b81cd72796a148
thomasw <thomasw@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-22 09:35:30 +0000

    pep8 compliance

--- slc/linguatools/browser/interfaces.py
+++ slc/linguatools/browser/interfaces.py
@@ -1,77 +1,77 @@
 from zope import interface, schema
-
 from z3c.form import button
 
+
 class INamingSchema(interface.Interface):
-    """ Base Schema for the edit form. It is dynamically extended by plugins """
+    """ Base Schema for the edit form. It is dynamically extended by plugins
+    """
     text = schema.Text(
             title=u"Text",
-            description= \
-                u"Type some text. This text will then be written on all translations as Title, as Description"
-                " or as a translation of a message id, depending on your further choices in this form.",
-            required=True
+            description=u"Type some text. This text will then be written on "\
+                u"all translations as Title, as Description or as a " \
+                u"translation of a message id, depending on your further "\
+                u"choices in this form.",
+            required=True,
             )
 
     po_domain = schema.TextLine(
             title=u"PO Domain",
-            description= \
-                u"Give a po file domain here, if you have typed a message id in the field above. Then its translation "
-                "will be written. If you leave the domain empty or state a nonexisting one, the text above will be written as-is.",
+            description=u"Give a po file domain here, if you have typed a "\
+                u"message id in the field above. Then its translation will " \
+                u"be written. If you leave the domain empty or state a non-" \
+                u"existing one, the text above will be written as-is.",
             default=u"plone",
-            required=False
+            required=False,
             )
 
     set_title = button.Button(title=u'Set text as Title')
     set_description = button.Button(title=u'Set text as Description')
+
 
 class IObjectHandlingSchema(interface.Interface):
     """ object handling """
 
     old_id = schema.Choice(
             title=u"Object to rename",
-            description= \
-                u"Choose an object to rename. The drop-down displays the available objects with title " \
-                u"and id in bracktets.",
+            description=u"Choose an object to rename. The drop-down displays "\
+                u"the available objects with title and id in bracktets.",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.available_ids"
+            vocabulary="slc.linguatools.vocabularies.available_ids",
             )
 
     new_id = schema.TextLine(
             title=u"New id",
-            description= \
-                u"Enter the id (short name) the object should receive.",
-            required=False
+            description=u"Enter the id (short name) the object should "\
+                u"receive.",
+            required=False,
             )
-    
 
     id_to_delete = schema.Choice(
             title=u"Object to delete",
-            description= \
-                u"Select an object that should be deleted in all languages.",
+            description=u"Select an object that should be deleted in all "\
+                u"languages.",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.available_ids"
+            vocabulary="slc.linguatools.vocabularies.available_ids",
             )
 
     id_to_move = schema.Choice(
             title=u"Object to move",
-            description= \
-                u"Choose an object to move.",
+            description=u"Choose an object to move.",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.available_ids"
+            vocabulary="slc.linguatools.vocabularies.available_ids",
             )
 
     target_path = schema.TextLine(
         title=u"Target path",
-        description=u"Enter either an absolute path or a path relative to the current location. " \
-            u"Examples: '/en/path/to/folder' (absolute); 'subfolder/from/here' or '../' (relative)",
-        required=False
+        description=u"Enter either an absolute path or a path relative to "\
+            u"the current location. Examples: '/en/path/to/folder' " \
+            u"(absolute); 'subfolder/from/here' or '../' (relative)",
+        required=False,
         )
-
 
     rename = button.Button(title=u'Rename')
     delete = button.Button(title=u'Delete')
     cut_and_paste = button.Button(title=u'Cut and paste')
-    
 
 
 class IPortletSchema(interface.Interface):
@@ -87,11 +87,12 @@ class IPortletSchema(interface.Interface):
 
     portlet_manager = schema.Choice(
             title=u"Portlet manager",
-            description= \
-                u"Select a portlet manager. It is used to determine where to block/unblock portlets on, or which "
-                u"portlets should be propagated. Leave unselected to do the action for all portlet slots.",
+            description=u"Select a portlet manager. It is used to determine "\
+                u"where to block/unblock portlets on, or which portlets "\
+                u"should be propagated. Leave unselected to do the action "\
+                u"for all portlet slots.",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.portletmanagers"
+            vocabulary="slc.linguatools.vocabularies.portletmanagers",
             )
 
 
@@ -103,10 +104,10 @@ class ISubtyperSchema(interface.Interface):
 
     subtype = schema.Choice(
             title=u"Available Subtypes",
-            description= \
-                u"Use this to subtype the object and its translations.",
+            description=u"Use this to subtype the object and its "\
+                u"translations.",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.subtypes"
+            vocabulary="slc.linguatools.vocabularies.subtypes",
             )
 
 
@@ -124,34 +125,35 @@ class IDuplicaterSchema(interface.Interface):
     """ Schema for object duplication"""
     translate_this = button.Button(title=u'Translate this')
 
-
     attributes_to_copy = schema.List(title=u'Attributes to copy',
-                            description=\
-                                u'Select one or more attributes to have their values copied over to the translations',
-                            default=list(),
-                            required=False,
-                            value_type=schema.Choice(
-                                vocabulary="slc.linguatools.vocabularies.translatable_fields",
-                            )
+            description=u'Select one or more attributes to have their values '\
+                u'copied over to the translations',
+            default=list(),
+            required=False,
+            value_type=schema.Choice(
+                vocabulary="slc.linguatools.vocabularies.translatable_fields",
+                ),
             )
 
     target_languages = schema.List(title=u'Target languages',
-                            description=\
-                                u'Select the languages to which you want to make a copy of the current object. ' \
-                                'Leave blank to select all available languages.',
-                            default=list(),
-                            required=False,
-                            value_type=schema.Choice(
-                                vocabulary="slc.linguatools.vocabularies.supported_languages",
-                            )
+            description=u'Select the languages to which you want to make a '\
+                u'copy of the current object. Leave blank to select all' \
+                u' available languages.',
+            default=list(),
+            required=False,
+            value_type=schema.Choice(
+                vocabulary="slc.linguatools.vocabularies.supported_languages",
+                ),
             )
 
     translation_exists = schema.Bool(
             title=u"Translation exists",
-            description=u"Tick this box if a translation alreay exits and you just want to propagate " \
-                u"attributes or Collection criteria",
+            description=u"Tick this box if a translation alreay exits and "\
+                u"you just want to propagate attributes or Collection "\
+                u"criteria",
             required=False,
             )
+
 
 class IPropertySchema(interface.Interface):
     """ Schema for setting and removing properties """
@@ -159,29 +161,27 @@ class IPropertySchema(interface.Interface):
     property_id = schema.TextLine(
         title=u"Property id",
         description=u"Enter a property id",
-        required=False
+        required=False,
         )
 
     property_type = schema.Choice(
             title=u"Property type",
-            description= \
-                u"Select the correct property type",
+            description=u"Select the correct property type",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.available_property_types"
+            vocabulary="slc.linguatools.vocabularies.available_property_types",
             )
 
     property_value = schema.TextLine(
         title=u"Property value",
         description=u"Enter a value fot the property",
-        required=False
+        required=False,
         )
 
     property_to_delete = schema.Choice(
             title=u"Property to delete",
-            description= \
-                u"Select a property to delete",
+            description=u"Select a property to delete",
             required=False,
-            vocabulary="slc.linguatools.vocabularies.available_property_ids"
+            vocabulary="slc.linguatools.vocabularies.available_property_ids",
             )
 
     set_property = button.Button(title=u'Set property')
