2a46e8d1f7e8d5eb400280b163cb5fd5b8f6eac1
thomasw <thomasw@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-02-04 13:24:43 +0000

    Added Robert Niederreiter's patch to TinyMCE's getBreadcrumbs() to enable common language neutral folders (e.g. for images)

--- slc/linguatools/__init__.py
+++ slc/linguatools/__init__.py
@@ -3,6 +3,9 @@ try:
 except ImportError:
     ISubtyper = None
 
+import os
+if bool(os.environ.get('PATCH_TINYMCE_BREADCRUMBS', False)):
+    import patch_tinymce_breadcrumbs
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
