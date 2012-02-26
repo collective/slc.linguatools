4d5e6848025e0f5b1c205539669129003bfcaf13
thomasw <thomasw@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-02-26 15:21:55 +0000

    linted awkward indentation

--- setup.py
+++ setup.py
@@ -25,53 +25,53 @@ long_description = (
     + '\n')
 
 requires = [
-          'setuptools',
-          'Products.LinguaPlone',
-          'plone.app.z3cform',
-          'plone.browserlayer',
-          'zope.i18n',
-          'z3c.form',
-          'zope.testing',
-          'zope.app.schema'
-      ]
+        'setuptools',
+        'Products.LinguaPlone',
+        'plone.app.z3cform',
+        'plone.browserlayer',
+        'zope.i18n',
+        'z3c.form',
+        'zope.testing',
+        'zope.app.schema',
+    ]
 
 install_requires = requires
 
 tests_require = requires +\
-               ['zope.testing']
+            ['zope.testing']
 
 setup(name='slc.linguatools',
-      version=version,
-      description="A set of tools that simplify handling multilingual "\
-        "content in Plone",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?
-      # %3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "License :: OSI Approved :: European Union Public Licence "\
-            "1.1 (EUPL 1.1)",
-        ],
-      keywords='linguatools internationalization linguaplone',
-      author='Syslab.com GmbH',
-      author_email='thomas@syslab.com',
-      url='http://pypi.python.org/pypi/slc.linguatools/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['slc'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='slc.linguatools.tests.test_docs.test_suite',
-      entry_points="""
-        [z3c.autoinclude.plugin]
-        target = plone
-      """,
-      )
+    version=version,
+    description="A set of tools that simplify handling multilingual "\
+    "content in Plone",
+    long_description=long_description,
+    # Get more strings from http://www.python.org/pypi?
+    # %3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Framework :: Zope2",
+    "Framework :: Zope3",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "License :: OSI Approved :: European Union Public Licence "\
+        "1.1 (EUPL 1.1)",
+    ],
+    keywords='linguatools internationalization linguaplone',
+    author='Syslab.com GmbH',
+    author_email='thomas@syslab.com',
+    url='http://pypi.python.org/pypi/slc.linguatools/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['slc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite='slc.linguatools.tests.test_docs.test_suite',
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
