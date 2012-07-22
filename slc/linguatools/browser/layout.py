1f088cc93783f2c1a333f1829cc3427b491fc229
Wolfgang Thomas <thomas@syslab.com>
2012-07-22 09:21:35 +0200

    direct import

--- slc/linguatools/browser/layout.py
+++ slc/linguatools/browser/layout.py
@@ -1,8 +1,8 @@
 import os
-import plone.z3cform
-
+from plone.z3cform.templates import ZopeTwoFormTemplateFactory
 from slc.linguatools.browser.linguatools_view import LinguaToolsView
 
-linguatools_layout = plone.z3cform.templates.ZopeTwoFormTemplateFactory(
+
+linguatools_layout = ZopeTwoFormTemplateFactory(
     os.path.join(os.path.dirname(__file__), 'templates/layout.pt'),
     form=LinguaToolsView)
